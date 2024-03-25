8f8226271b54114a97258980c9af0600ee7864c8
Srinivas Talla <tallasrinivas86@yahoo.com>
2024-03-25 21:21:38 +0000

    Json Report

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -8,7 +8,7 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         features = "src/test/resources/features",
         glue = "org.amazon.stepdefs",
-        plugin = {"pretty", "html:target/cucumber.html"}
+        plugin = {"pretty", "html:target/cucumber.html","json:target/cucumber.json"}
 )
 public class TestRunner {
 }
